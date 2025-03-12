cc1bdefcdec290506ee0dc3c2a155ec0a1c49994
Srikanth-HS <165276815+Srikanth-HS@users.noreply.github.com>
2025-03-12 12:35:55 +0530

    Commented function

--- Statistics.Test/StatsUnitTest.cs
+++ Statistics.Test/StatsUnitTest.cs
@@ -13,21 +13,21 @@ namespace Statistics.Test
             var statsComputer = new StatsComputer();
             var computedStats = statsComputer.CalculateStatistics(
                 new List<float>{1.5, 8.9, 3.2, 4.5});
-            float epsilon = 0.001F;
+            float epsilon = 0.001F;            
             Assert.True(Math.Abs(computedStats.average - 4.525) <= epsilon);
             Assert.True(Math.Abs(computedStats.max - 8.9) <= epsilon);
             Assert.True(Math.Abs(computedStats.min - 1.5) <= epsilon);
         }
-        [Fact]
-        public void ReportsNaNForEmptyInput()
-        {
-            var statsComputer = new StatsComputer();
+        // [Fact]
+        // public void ReportsNaNForEmptyInput()
+        // {
+            //var statsComputer = new StatsComputer();
             // var computedStats = statsComputer.CalculateStatistics(
             //     new List<___>{});
             // All fields of computedStats (average, max, min) must be
             // Double.NaN (not-a-number), as described in
             // https://docs.microsoft.com/en-us/dotnet/api/system.double.nan?view=netcore-3.1
             // Specify the Assert statements here
-        }
+        //}
     }
 }
