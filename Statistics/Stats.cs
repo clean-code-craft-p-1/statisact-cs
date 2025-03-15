b573cc2c26e92010d16c74b916b747ae8db00fd6
Swarastra Bhusan Mishra <320103832@INGBTCPIC6NB530>
2025-03-16 02:30:49 +0530

    [Update] Added Implimentation and Corrected the TestCase

---
+++ Statistics/Stats.cs
@@ -0,0 +1,15 @@
+﻿namespace Statistics
+{
+    public class Stats
+    {
+        public Stats(float avg, float max, float min)
+        {
+            Average = avg;
+            Max = max;
+            Min = min;
+        }
+        public float Average { get; set; }
+        public float Max { get; set; }
+        public float Min { get; set; }
+    }
+}
